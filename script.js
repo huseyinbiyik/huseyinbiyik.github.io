9a389ba97c2ba767b4dd615e7e5ba4fbabc920bb
ndayishimiyeeric <82580160+ndayishimiyeeric@users.noreply.github.com>
2022-04-04 17:44:27 +0200

    Add toggling functionality

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const menuBtn = document.querySelector('.hamburger-menu')
 const mobileNav = document.querySelector('.nav-bar')
+
+menuBtn.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+    document.body.classList.toggle('overflow-disabled')
+})
+
+const navlinks = document.getElementsByClassName('link');
+
+for (i = 0; i < navlinks.length; i++){
+    navlinks[i].addEventListener('click', () => {
+        mobileNav.classList.toggle('active'); 
+        document.body.classList.toggle('overflow-disabled')
+    })
+}
